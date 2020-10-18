1347377f4579d321fc5e1e056b767cebeffb67a4
An Hoang <anhoang31415@gmail.com>
2020-10-18 17:52:04 +0100

    add type annotations

--- src/transcribe.py
+++ src/transcribe.py
@@ -5,7 +5,8 @@ import time
 import json
 import logging
 
-def move_file_to_bucket(file_dir, bucket_name):
+
+def move_file_to_bucket(file_dir: str, bucket_name: str) -> str:
     logging.info(f"Move {file_dir} to the bucket {bucket_name}.")
     client = boto3.resource('s3')
     bucket = client.Bucket(bucket_name)
@@ -14,12 +15,11 @@ def move_file_to_bucket(file_dir, bucket_name):
     return os.path.basename(file_dir)
 
 
-def remove_file_from_bucket(key, bucket_name):
+def remove_file_from_bucket(key: str, bucket_name: str) -> None:
     client = boto3.resource('s3')
     bucket = client.Bucket(bucket_name)
     bucket.delete_objects(
-        Delete={
-            'Objects': [
+        Delete={ 'Objects': [
                 {
                     'Key': key,
                 },
@@ -28,7 +28,7 @@ def remove_file_from_bucket(key, bucket_name):
     )
 
 
-def read_and_delete_transcript_from_s3(key, bucket_name):
+def read_and_delete_transcript_from_s3(key: str, bucket_name: str) -> str:
     logging.info(f"Read json file {key} from the bucket {bucket_name}.")
     client = boto3.client('s3')
     result = client.get_object(Bucket=bucket_name, Key=key)
@@ -37,7 +37,7 @@ def read_and_delete_transcript_from_s3(key, bucket_name):
     return response['results']['transcripts'][0]['transcript']
 
 
-def transcribe_file(file_dir, bucket_name, language="en-US"):
+def transcribe_file(file_dir: str, bucket_name: str, language: str ="en-US") -> str:
     job_name = str(uuid.uuid4())
     logging.info(
         f"Start the transcript job {job_name} with the file from {file_dir}.")
